b8854fd664cbbaeb883b3f2d0229ca49bfca2154
abdullahcoding123 <abdullahshahzadturk@gmail.com>
2026-01-07 20:07:36 +0500

    Loops

---
+++ homework.py
@@ -0,0 +1,6 @@
+print("POWER CALCULATOR")
+number=int(input("Enter any number you want to take the square= "))
+power=int(input("Enter any number= "))
+result=number**power
+print("Your answer is =",result)
+print("Best Of Luck!")
